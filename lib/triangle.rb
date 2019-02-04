0eda50e4bb525471c889dcde75814596d631e724
Maximo Bautista <max.bautista97@gmail.com>
2019-02-04 21:59:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
  
   def kind
     
-    if @side1 + @side2 < @side3 || @side1 + @side3 < @side2 ||  @side2 + @side3 < @side1
+    if @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 ||  @side2 + @side3 <= @side1
       begin
         raise TriangleError
       end  
